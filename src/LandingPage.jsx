224b2d6efd718e85f6f45177bc398e71d182ab73
George Udonte <georgeudonte@gmail.com>
2024-02-09 11:10:59 +0100

    added icons

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import { Link } from "react-scroll";
+import { FaArrowRightLong, FaGithub } from "react-icons/fa6";
 
 const LandingPage = () => {
   return (
@@ -53,11 +54,13 @@ const LandingPage = () => {
             <span className="text-[#91D2F7]">out-of-the-box.</span>
           </p>
           <div className="flex items-center justify-center text-center gap-8 my-8">
-            <button className="bg-[#FFFF00] text-black rounded py-2 px-4 font-bold text-xl">
+            <button className="bg-[#FFFF00] text-black rounded py-2 px-4 font-bold text-xl flex items-center gap-2">
               Get Started
+              <FaArrowRightLong />
             </button>
-            <button className="bg-black text-[#FFFF00] border-[#FFFF00] border-[1px] rounded py-2 px-4 font-bold text-xl">
+            <button className="bg-black text-[#FFFF00] border-[#FFFF00] border-[1px] rounded py-2 px-4 font-bold text-xl flex items-center gap-2">
               See Repo
+              <FaGithub />
             </button>
           </div>
         </div>
@@ -69,33 +72,34 @@ const LandingPage = () => {
           <div className="flex flex-col items-center justify-center w-[850] gap-8 my-8">
             <div className="flex items-center gap-8">
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Amazon
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Apple
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Microsoft
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Nike
               </div>
             </div>
             <div className="flex items-center gap-8">
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Disney
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Coca-Cola
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Samsung
               </div>
               <div className="bg-gray-300 text-black text-3xl font-bold py-2 px-4">
-                Google
+                Toyota
               </div>
             </div>
           </div>
+
           <div className="w-[513px] text-center flex flex-col justify-center items-center">
             <p className="text-3xl font-bold">Less code. More product.</p>
             <p>
@@ -112,55 +116,44 @@ const LandingPage = () => {
           <p className="font-bold text-4xl mb-8">About</p>
           <div>
             <p className="font-bold">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Dolor
-              sit amet consectetur adipiscing elit pellentesque habitant. Cras
-              pulvinar mattis nunc sed blandit libero volutpat. Odio ut enim
-              blandit volutpat maecenas volutpat. Sapien eget mi proin sed
-              libero enim. Amet facilisis magna etiam tempor. Facilisi cras
-              fermentum odio eu feugiat pretium nibh. Nibh tellus molestie nunc
-              non blandit. Integer enim neque volutpat ac tincidunt vitae semper
-              quis. Gravida rutrum quisque non tellus. Et pharetra pharetra
-              massa massa ultricies mi quis. Nisl rhoncus mattis rhoncus urna
-              neque viverra justo nec. Eget sit amet tellus cras adipiscing enim
-              eu turpis. Volutpat commodo sed egestas egestas fringilla. Dui
-              accumsan sit amet nulla. Morbi quis commodo odio aenean sed
-              adipiscing. Elementum nisi quis eleifend quam adipiscing vitae
-              proin sagittis nisl. Interdum posuere lorem ipsum dolor. Dolor
-              magna eget est lorem ipsum dolor. Nibh nisl condimentum id
-              venenatis.
+              Welcome to JustAnotherUIKit, where innovation meets convenience.
+              As passionate creators ourselves, we understand the importance of
+              having the right tools at your fingertips. That's why we've
+              curated a diverse collection of kits designed to inspire and
+              empower. Whether you're a seasoned professional or just starting
+              your creative journey, our carefully selected products cater to
+              all skill levels and interests. From DIY enthusiasts to seasoned
+              artisans, our extensive range includes everything from art
+              supplies and crafting essentials to specialized equipment and
+              innovative gadgets. With our commitment to quality and customer
+              satisfaction, you can trust JustAnotherUIKit to provide you with
+              the resources you need to unleash your creativity and bring your
+              ideas to life. Join our community today and embark on a journey of
+              endless possibilities.
               <br />
               <br />
-              Aenean vel elit scelerisque mauris pellentesque pulvinar
-              pellentesque habitant. Interdum consectetur libero id faucibus
-              nisl tincidunt eget nullam non. Mi bibendum neque egestas congue
-              quisque egestas diam in arcu. Fermentum odio eu feugiat pretium
-              nibh ipsum consequat nisl vel. Arcu dictum varius duis at
-              consectetur lorem. In nibh mauris cursus mattis molestie. Velit ut
-              tortor pretium viverra suspendisse potenti nullam ac. Vivamus at
-              augue eget arcu dictum varius. Ullamcorper malesuada proin libero
-              nunc consequat interdum varius sit. Volutpat odio facilisis mauris
-              sit amet massa vitae tortor condimentum. Morbi quis commodo odio
-              aenean sed adipiscing diam donec. Nullam vehicula ipsum a arcu.
-              Arcu non odio euismod lacinia at. Odio tempor orci dapibus
-              ultrices in iaculis.
+              We're dedicated to providing top-quality products, ensuring
+              satisfaction every step of the way. Our mission is to empower you
+              with the tools you need to excel in your endeavors. From crafting
+              essentials to specialized gear, we've got you covered. Join us on
+              this journey and unlock the potential within. Whether you're an
+              artist, crafter, DIY enthusiast, or hobbyist, our diverse range of
+              kits has something for everyone. From painting and sculpting
+              supplies to crafting essentials and tech gadgets, we've got you
+              covered. Our curated selection features top brands, innovative
+              products, and exclusive items you won't find anywhere else.
               <br />
               <br />
-              Felis eget velit aliquet sagittis id consectetur purus ut
-              faucibus. Est velit egestas dui id. Ipsum a arcu cursus vitae
-              congue mauris rhoncus aenean vel. Sed felis eget velit aliquet
-              sagittis id consectetur. Consequat interdum varius sit amet
-              mattis. Adipiscing elit pellentesque habitant morbi tristique.
-              Elit eget gravida cum sociis natoque penatibus et magnis dis. Sit
-              amet nisl purus in mollis nunc. In nulla posuere sollicitudin
-              aliquam. Ut tortor pretium viverra suspendisse potenti. Facilisi
-              etiam dignissim diam quis enim. Consectetur purus ut faucibus
-              pulvinar elementum integer enim neque volutpat. Pellentesque massa
-              placerat duis ultricies lacus. Donec massa sapien faucibus et
-              molestie. Ac turpis egestas sed tempus urna et pharetra. At lectus
-              urna duis convallis convallis tellus. Egestas diam in arcu cursus
-              euismod quis viverra. Malesuada nunc vel risus commodo viverra
-              maecenas accumsan lacus vel.
+              But we're more than just a store – we're a hub for creativity and
+              community. Join us on social media to connect with fellow
+              creators, share your projects, and discover new ideas. Plus, with
+              our expert tips, tutorials, and resources, you'll have everything
+              you need to unleash your creativity and take your skills to the
+              next level. At JustAnotherUIKit, we believe that everyone has the
+              potential to create something amazing. So whether you're a
+              seasoned pro or just starting out, we're here to support you every
+              step of the way. Welcome to our community – let's make something
+              incredible together!
             </p>
           </div>
         </div>
@@ -224,45 +217,45 @@ const LandingPage = () => {
             <div className="flex items-start justify-center gap-24">
               <div>
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">JustAnotherKit</li>
+                  <li className="no-underline">About Us</li>
+                  <li className="no-underline">Our Clients</li>
+                  <li className="no-underline">Services</li>
+                  <li className="no-underline">Contact Us</li>
+                  <li className="no-underline">FAQs</li>
                 </ul>
               </div>
               <div className="flex flex-col justify-start gap-2">
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">Blog</li>
+                  <li className="no-underline">Terms of Service</li>
                 </ul>
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">Privacy Policy</li>
+                  <li className="no-underline">Shipping Information</li>
+                  <li className="no-underline">Returns & Exchanges</li>
                 </ul>
               </div>
               <div>
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">Customer Reviews</li>
+                  <li className="no-underline">Affiliate Program</li>
+                  <li className="no-underline">Gift Cards</li>
+                  <li className="no-underline">Careers</li>
+                  <li className="no-underline">Press</li>
+                  <li className="no-underline">Accessibility</li>
                 </ul>
               </div>
               <div className="flex flex-col justify-start gap-2">
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">Social Media</li>
+                  <li className="no-underline">Partnerships</li>
+                  <li className="no-underline">Events</li>
                 </ul>
                 <ul className="">
-                  <li className="font-bold no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
-                  <li className="no-underline">lorem</li>
+                  <li className="font-bold no-underline">Customer Support</li>
+                  <li className="no-underline">Product Care</li>
+                  <li className="no-underline">Sustainability</li>
                 </ul>
               </div>
             </div>
